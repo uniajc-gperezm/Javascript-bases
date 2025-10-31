e44f76740a8c78f33e1c49c1855ffcb712681342
Gabriel Perez <gperezm@profesores.uniajc.edu.co>
2025-10-30 19:42:13 -0500

    proyecto node js prueba

---
+++ index.js
@@ -0,0 +1,18 @@
+// NVM - Node version manager
+
+// Node JS = Javascript del lado del server
+
+// NPM = Node package manager
+// Instalar dependencias o librerias de tercero
+
+import chalk from 'chalk';
+import { usuario, docente } from './modulo.js';
+
+console.log(chalk.red("Hola desde Node JS"));
+console.log(chalk.blue("Hola desde Node JS"));
+console.log(chalk.yellow("Hola desde Node JS"));
+console.log(chalk.green("Hola desde Node JS"));
+
+console.log("Usuario:", usuario);
+
+console.log("Docente:", docente);
---
+++ modulo.js
@@ -0,0 +1,11 @@
+export const usuario = {
+    nombre: "Usuario Ana",
+    edad: 28,
+    pais: "Colombia"
+};
+
+export const docente = {
+    nombre: "Docente Juan",
+    edad: 28,
+    pais: "Colombia"
+};
